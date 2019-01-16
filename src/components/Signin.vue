8a11db6d324bd47a7a0200d1b75a85a1df031ed0
sunecosuri <sunecosuri@gmail.com>
2019-01-16 12:33:38 +0900

    singin とtask-pageを更新

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,8 +1,46 @@
 <template>
   <div id="signin">
-    サインイン
+    <h2>サインイン</h2>
+    <!-- emailとpasswordの入力欄 -->
+    <input type="email" placeholder="email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button @click="signIn">Signin</button>
+    <!-- サインアップページ遷移ボタン -->
+    <p>
+      アカウントをお持ちではない方
+      <router-link to="/signup">新規作成</router-link>
+    </p>
   </div>
 </template>
+
+<script>
+import firebase from "firebase" // firebaseのインポート
+
+export default {
+  name: "Signin",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    // signinボタンを押したときに実行されるfunction
+    signIn: async function() {
+      try {
+      const res = await firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+      alert(`ログインしました： ${res.user.email}`)
+      this.$router.push("/")
+      } catch (error) {
+        alert(error.message)
+      }
+    }
+  }
+};
+</script>
+
 
 <style>
 </style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,94 @@
 <template>
-  <div id="task">
-      タスクページ
+  <div class="task">
+     <h2>タスク</h2>
+    <div>
+      <input type="text" v-model="newTodoName">
+      <button type="submit" @click="createTodo()">タスク作成</button>
+    </div>
+    <ul>
+      <li><button type="submit" @click="showTodoType = 'all'">すべて</button></li>
+      <li><button type="submit" @click="showTodoType = 'active'">未完タスク一覧</button></li>
+      <li><button type="submit" @click="showTodoType = 'complete'">完了タスク一覧</button></li>
+    </ul>
+    <!-- todoの一覧表示 -->
+    <ul v-for="(todo, key) in filteredTodos" :key="todo.id">
+      <li><input class="toggle" type="checkbox" v-model="todo.isComplete" @click="updateIsCompleteTodo(todo, key)">{{ todo.name }}</li>
+      <button type="submit" @click="deleteTodo(key)">削除</button>
+    </ul>
   </div>
 </template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  name: "Task",
+  data() {
+    return {
+      database: null,
+      todosRef: null,
+      newTodoName: "",
+      showTodoType: "all",
+      todos: []
+    }
+  },
+  created: function() {
+    this.database = firebase.database()
+    this.uid = firebase.auth().currentUser.uid
+    this.todosRef = this.database.ref("todos/" + this.uid)
+
+    // データに変更があると実行されるfunction
+    this.todosRef.on("value", (snapshot) => {
+      this.todos = snapshot.val() // 再取得してtodosに格納する
+    })
+  },
+  computed: {
+    // フィルターの実装showTodoTypeが変更されると実行される
+    filteredTodos: function() {
+      if (this.showTodoType == "all") {
+        return this.todos
+      } else {
+        let showIsComplete = false
+        if (this.showTodoType == "complete") {
+          showIsComplete = true
+        }
+        let filterTodos = {}
+        for (let key in this.todos) {
+          let todo = this.todos[key]
+          if (todo.isComplete == showIsComplete) {
+            filterTodos[key] = todo
+          }
+        }
+        return filterTodos
+      }
+    }
+  },
+  methods: {
+    // DBのtodos/[uid]/以下にデータを格納していく
+    createTodo: function() {
+      if (this.newTodoName == "") {
+        return
+      }
+      this.todosRef.push({
+        name: this.newTodoName,
+        isComplete: false
+      })
+      this.newTodoName = ""
+    },
+    // 完了・未完了の値の更新
+    updateIsCompleteTodo: function(todo, key) {
+      todo.isComplete = !todo.isComplete
+      const updates = {}
+      updates[key] = todo
+      this.todosRef.update(updates)
+    },
+    // todoの削除
+    deleteTodo: function(key) {
+      this.todosRef.child(key).remove()
+    }
+  }
+}
+</script>
 
 <style>
 </style>
