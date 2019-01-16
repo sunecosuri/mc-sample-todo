d4815ab0e5b3cac2ea928a72350ef770791b4f92
sunecosuri <sunecosuri@gmail.com>
2019-01-16 14:09:42 +0900

    dotenv 外した

--- src/main.js
+++ src/main.js
@@ -4,17 +4,9 @@ import router from './router'
 import firebase from 'firebase'
 
 Vue.config.productionTip = false
-const dotenv = require('dotenv')
-dotenv.config()
-const config = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
-}
+// ここにfirebaseの各情報を貼る
 
+//
 firebase.initializeApp(config)
 
 new Vue({
