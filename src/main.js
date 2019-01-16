7e5f33e4d7d15e9a76645bacb0c64b459f3ae17f
sunecosuri <sunecosuri@gmail.com>
2019-01-16 10:02:51 +0900

    firebase をenvから参照するように変更

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+const dotenv = require('dotenv')
+dotenv.config()
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config)
 
 new Vue({
   router,
