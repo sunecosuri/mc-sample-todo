f7c0b4ce84997088af259415c2542b3ecfb19eef
sunecosuri <sunecosuri@gmail.com>
2019-01-16 09:42:51 +0900

    beforeEach時に認証情報を確認する処理を追加

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Task from '@/components/Task.vue'
 import Signup from '@/components/Signup'
 import Signin from '@/components/Signin'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
@@ -25,6 +26,27 @@ let router = new Router({
       component: Signin
     }
   ]
+})
+
+
+// ログインが完了していない場合にサインインへリダイレクトさせる
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  // firebaseから認証情報を取得できるか確認する
+  const currentUser = firebase.auth().currentUser
+  if (requiresAuth) {
+    if (!currentUser) {
+      // singin へリダイレクト
+      next({
+        path: '/signin',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 
